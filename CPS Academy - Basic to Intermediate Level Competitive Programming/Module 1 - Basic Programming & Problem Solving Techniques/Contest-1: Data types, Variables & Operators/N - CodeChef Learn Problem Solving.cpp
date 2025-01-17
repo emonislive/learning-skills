4641e2638cc3235d2cea4fee5562da83b2a12b90
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2025-01-18 02:04:53 +0600

    18/01/2025

---
+++ CPS Academy - Basic to Intermediate Level Competitive Programming/Module 1 - Basic Programming & Problem Solving Techniques/Contest-1: Data types, Variables & Operators/N - CodeChef Learn Problem Solving.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#define endl '\n'
+#define ll long long
+
+void solve(ll a)
+{
+    cout << a * 2 << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll a;
+    cin >> a;
+    solve(a);
+    return 0;
+}
