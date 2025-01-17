235d41b26236215f79508a8ffb017caebc7066c6
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2025-01-18 02:37:49 +0600

    18/01/2025

---
+++ CPS Academy - Basic to Intermediate Level Competitive Programming/Module 1 - Basic Programming & Problem Solving Techniques/Contest-1: Data types, Variables & Operators/T - Leyland Number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+#define endl '\n'
+#define ll long long
+
+void solve(ll a, ll b)
+{
+    ll ans = pow(a, b) + pow(b, a);
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll a, b;
+    cin >> a >> b;
+    solve(a, b);
+    return 0;
+}
