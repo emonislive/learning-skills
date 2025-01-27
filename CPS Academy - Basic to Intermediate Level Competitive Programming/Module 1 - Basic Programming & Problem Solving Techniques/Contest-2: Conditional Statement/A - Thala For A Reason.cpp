04040cc7fae57a76fe9dfab9b4d29b50733b8532
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2025-01-27 13:52:43 +0600

    27/01/2025

---
+++ CPS Academy - Basic to Intermediate Level Competitive Programming/Module 1 - Basic Programming & Problem Solving Techniques/Contest-2: Conditional Statement/A - Thala For A Reason.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+void solve(int n)
+{
+    if (n == 7)
+        cout << "THALA" << endl;
+    else
+        cout << "SADGE" << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    solve(n);
+    return 0;
+}
