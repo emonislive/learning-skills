3c63e37b2f248f6c200789223830b491d5092d70
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2025-02-05 21:20:36 +0600

    05/02/2025

---
+++ CPS Academy - Basic to Intermediate Level Competitive Programming/Module 1 - Basic Programming & Problem Solving Techniques/Contest-3: Loops/A - A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+void solve()
+{
+    int age;
+    cin >> age;
+    cout << age - 10 << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int testCase;
+    cin >> testCase;
+    while (testCase--)
+    {
+        solve();
+    }
+    return 0;
+}
