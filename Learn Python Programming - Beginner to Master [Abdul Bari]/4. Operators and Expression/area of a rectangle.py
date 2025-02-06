1570aff552c56ea712e720f56e493cdc95f30af1
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2025-02-06 21:29:41 +0600

    06/02/2025

---
+++ Learn Python Programming - Beginner to Master [Abdul Bari]/4. Operators and Expression/area of a rectangle.py
@@ -0,0 +1,7 @@
+# ! Area of a rectangle
+
+length = input("Enter the length of a rectangle: ")
+breath = input("Enter the breath of a rectangle: ")
+
+area = float(length) * float(breath)
+print("Area of the rectangle:",area)
