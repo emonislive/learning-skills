7ee07f7d7d73f18cb43fb4bb663acd3e3c7d56b7
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2025-02-06 21:35:38 +0600

    06/02/2025

---
+++ Learn Python Programming - Beginner to Master [Abdul Bari]/4. Operators and Expression/rhombus area.py
@@ -0,0 +1,8 @@
+# ! Rhombus area
+
+height = input("Enter the height: ")
+base = input("Enter the base: ")
+area = input("Enter the area: ")
+
+rArea = ((float(area) + float(base)) * float(height)) / 2
+print("Rhombus area:",rArea)
