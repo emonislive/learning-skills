587f3d43a9b7cf9583f9acf97326d46db869ea18
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2025-02-06 22:20:57 +0600

    06/02/2025

---
+++ Learn Python Programming - Beginner to Master [Abdul Bari]/4. Operators and Expression/total surface area of a cuboid.py
@@ -0,0 +1,7 @@
+length = float(input("Enter length: "))
+breadth = float(input("Enter breadth: "))
+height = float(input("Enter height: "))
+
+area = 2 * ((length * height) + (length * breadth) + (breadth * height))
+
+print("Total surface area of a cuboid: ", area)
