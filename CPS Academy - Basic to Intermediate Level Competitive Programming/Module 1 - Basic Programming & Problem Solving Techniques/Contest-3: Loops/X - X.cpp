adb048c4033d397950117a30cbbd747e4a1b6cf3
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2025-02-14 18:30:35 +0600

    14/02/2025

---
+++ CPS Academy - Basic to Intermediate Level Competitive Programming/Module 1 - Basic Programming & Problem Solving Techniques/Contest-3: Loops/X - X.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define ld long double
+#define fast_io() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+
+void solve() {
+    ll count = 1;
+    ld insuranceCost, commission, insuranceSold;
+
+    cin >> insuranceCost;
+    commission = insuranceCost * 0.2;
+
+    for (ll i = 1; ;i++) {
+        insuranceSold = commission * i;
+
+        if (insuranceSold > 100)
+            break;
+        else
+            count++;
+    }
+    cout << count << endl;
+}
+
+int main() {
+    fast_io();
+    int testCase;
+    cin >> testCase;
+    while (testCase--) {
+        solve();
+    }
+    return 0;
+}
