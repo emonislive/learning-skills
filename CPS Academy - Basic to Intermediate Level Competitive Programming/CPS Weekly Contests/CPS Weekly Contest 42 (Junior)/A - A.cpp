739a59419d24cc07c73bbd2c41745ef9ace35f1c
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2025-02-21 21:57:24 +0600

    21/02/2025

---
+++ CPS Academy - Basic to Intermediate Level Competitive Programming/CPS Weekly Contests/CPS Weekly Contest 42 (Junior)/A - A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+
+void solve() {
+  ll n;
+  cin >> n;
+  
+  if (n >= 2000)
+    cout << "YES" << endl;
+  else
+    cout << "NO" << endl;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll tt;
+  cin >> tt;
+  while (tt--) {
+    solve();
+  }
+  return 0;
+}
