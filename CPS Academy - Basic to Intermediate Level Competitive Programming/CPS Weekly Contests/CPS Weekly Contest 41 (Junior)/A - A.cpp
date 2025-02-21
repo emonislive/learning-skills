f4789cc302a6d4f13adf76731da2a01d7331b8f1
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2025-02-22 00:01:30 +0600

    21/02/2025

---
+++ CPS Academy - Basic to Intermediate Level Competitive Programming/CPS Weekly Contests/CPS Weekly Contest 41 (Junior)/A - A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+
+void solve() {
+  ll chocolate, candy;
+  cin >> chocolate >> candy;
+
+  chocolate *= 2;
+  candy *= 5;
+
+  if (chocolate > candy)
+    cout << "Chocolate" << endl;
+  else if (chocolate < candy)
+    cout << "Candy" << endl;
+  else
+    cout << "Either" << endl;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll tt;
+  cin >> tt;
+  while (tt--) {
+    solve();
+  }
+  return 0;
+}
