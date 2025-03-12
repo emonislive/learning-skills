659a0be24d09a3a3f154c17eb2419bf31ba96337
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2025-03-13 01:23:49 +0600

    13/03/2025

---
+++ CPS Academy - Basic to Intermediate Level Competitive Programming/Module 1 - Basic Programming & Problem Solving Techniques/Contest-6: String/I - tcdr.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+void fastIO()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+void solve()
+{
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u')
+            cout << s[i];
+    }
+    cout << endl;
+}
+
+int main()
+{
+    fastIO();
+    solve();
+    return 0;
+}
