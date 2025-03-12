3a7660e9662c16e8040b3dcbc375cfc2a02b77ee
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2025-03-13 00:04:24 +0600

    13/03/2025

--- CPS Academy - Basic to Intermediate Level Competitive Programming/Module 1 - Basic Programming & Problem Solving Techniques/Contest-6: String/A - YES or YES?.cpp
+++ CPS Academy - Basic to Intermediate Level Competitive Programming/Module 1 - Basic Programming & Problem Solving Techniques/Contest-6: String/A - YES or YES?.cpp
@@ -1,1 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
 
+void fastIO()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+void solve()
+{
+    string s, s2;
+    cin >> s;
+
+    for (int i = 0; i < s.size(); i++)
+        s2.push_back(tolower(s[i]));
+
+    if (s2 == "yes")
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+
+int main()
+{
+    fastIO();
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
