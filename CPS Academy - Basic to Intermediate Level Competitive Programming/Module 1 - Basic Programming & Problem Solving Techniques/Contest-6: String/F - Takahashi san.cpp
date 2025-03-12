90144979be07228bf107c3fb48ebb08115a7e044
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2025-03-13 01:03:21 +0600

    13/03/2025

---
+++ CPS Academy - Basic to Intermediate Level Competitive Programming/Module 1 - Basic Programming & Problem Solving Techniques/Contest-6: String/F - Takahashi san.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+void fastIO()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+void solve()
+{
+    string s, t;
+    cin >> s >> t;
+    t = "san";
+    cout << s << " " << t << endl;
+}
+
+int main()
+{
+    fastIO();
+    solve();
+    return 0;
+}
