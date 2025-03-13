635b9e0bb5765b81aef8d6bcb0ce76694a9cfaf6
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2025-03-13 20:53:44 +0600

    13/03/2025

---
+++ CPS Academy - Basic to Intermediate Level Competitive Programming/Module 1 - Basic Programming & Problem Solving Techniques/Contest-6: String/R - Lucky Seven.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+void fastIO()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+void solve()
+{
+    string s;
+    cin >> s;
+
+    cout << s[6] << endl;
+}
+
+int main()
+{
+    fastIO();
+    solve();
+    return 0;
+}
